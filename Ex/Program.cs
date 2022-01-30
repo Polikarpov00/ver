05b8f13dfa63d8643b12dcd8f8dde457174445f1
German <polik20003@gmail.com>
2022-01-30 14:34:42 +0300

    added prog

---
+++ Ex/Program.cs
@@ -0,0 +1,28 @@
+﻿// Написать программу, которая из имеющегося массива строк формирует массив из строк, длина которых меньше либо равна 3.
+string[] array= new string[] {"one", "two", "three", "four", "five"}; 
+
+// string[] empty = new string[5]  ; 
+
+// int i;
+// int j;
+
+//  for( i = 0; i<array.Length ; i++ )
+//  {
+//      for ( j = 0; array[i].Length <=3; j++)   Я не понимаю как заполнить пустой массив((((
+    //      {
+//          empty[j] = array[i];
+//      } 
+     
+//       empty[j] =
+//     Console.WriteLine(empty[j]);
+//  }
+int i;
+
+ for( i = 0; i<array.Length ; i++ )
+ {
+     if(array[i].Length <= 3)
+     {
+        Console.WriteLine(array[i]); 
+     }
+
+ }
